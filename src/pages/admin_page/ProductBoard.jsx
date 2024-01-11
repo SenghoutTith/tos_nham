a95c4b12cf5600a0b32f83b364a59904bbe44d43
senghoutTith <senghout12.sh5@gmail.com>
2024-01-11 22:38:30 +0700

    formdata changes

--- src/pages/admin_page/ProductBoard.jsx
+++ src/pages/admin_page/ProductBoard.jsx
@@ -37,7 +37,7 @@ const ProductBoard = () => {
         const res = await createProductApiCall({...formData}).unwrap()
         refetch()
         setOpenCreate(false)
-        setFormData(initialState || {})
+        setFormData({})
         toast.success("Product added successfully")
       } catch (error) {
         toast.error(error?.data?.error || error.message)  
